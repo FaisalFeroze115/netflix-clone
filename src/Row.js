f72d505dd4201c93cad37783c643185ea611761f
Faisal Feroze <faisal.feroze.1995@gmail.com>
2021-05-16 12:00:29 +0600

    tmdn movie api integrated

---
+++ src/Row.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from './axios'
+import './row.css'
+
+const base_url = 'https://image.tmdb.org/t/p/original';
+const Row = ({title,fetchUrl}) => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(()=>{
+        const fetchData = async () =>{
+            const request = await axios.get(fetchUrl);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    },[fetchUrl])
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row__posters">
+                {movies.map(movie=>
+                    <img 
+                        key={movie.id}
+                        className="row__poster"
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name}
+                    />
+                )}
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Row
